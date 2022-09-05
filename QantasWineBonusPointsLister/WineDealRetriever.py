5c6c9ff6eef04b2ef6d54591020d0666d5bccb67
Garland <hu.garland@gmail.com>
2022-09-05 16:34:45 +1000

    Refactor Parser to be stateless

--- QantasWineBonusPointsLister/WineDealRetriever.py
+++ QantasWineBonusPointsLister/WineDealRetriever.py
@@ -6,14 +6,16 @@ class WineDealRetriever:
 		self._wine_deal_url = wine_deal_url
 		self._parser = parser
 		self._number_of_pages_to_scan = number_of_pages_to_scan
+		self._wine_deal_list = []
 		
 	def get_wine_list(self):
 		for counter in range(1, self._number_of_pages_to_scan):
 			page = requests.get(self._wine_deal_url.format(counter))
-			self._parser.feed(page.text)
-			if self._parser.done:
+			new_deals = self._parser.feed(page.text)
+			if new_deals:
+				self._wine_deal_list += new_deals
+			else:
 				break
+		
 		print(f'Scanned {counter - 1} pages of wine deals.')
-		if not self._parser.done:
-			print('Scan ended early')
-		return self._parser.wine_deal_list
+		return self._wine_deal_list
