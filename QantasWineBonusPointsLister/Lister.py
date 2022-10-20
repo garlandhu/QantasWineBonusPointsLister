9523d49dfd2d558b0b658729c5489c8662a76de1
garlandhu <hu.garland@gmail.com>
2022-10-20 11:02:10 +1100

    Update Lister.py

--- QantasWineBonusPointsLister/Lister.py
+++ QantasWineBonusPointsLister/Lister.py
@@ -7,7 +7,7 @@ class Lister:
 	def run(self, interactive, options_to_display):
 		wine_list = self._retriever.get_wine_list()
 		print(f'Found {len(wine_list)} deals')
-		ordered_wine_list = sorted(wine_list, key=lambda wine: wine.value, reverse=True)
+		ordered_wine_list = sorted(wine_list, key=lambda wine: wine.value)
 		
 		if not options_to_display:
 			options_to_display = int(input('How many shall I list? '))
