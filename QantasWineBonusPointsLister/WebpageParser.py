05e9618be37227226b27db7db3d2f168e9d390da
Garland Hu <garland.hu@wisetechglobal.com>
2024-11-20 14:10:08 +1100

    'a'

--- QantasWineBonusPointsLister/WebpageParser.py
+++ QantasWineBonusPointsLister/WebpageParser.py
@@ -4,8 +4,6 @@ import re
 
 
 class WebpageParser:
-	empty_page_class_title = "sc-hEBzJi fOfXwr"
-
 	def feed(self, html_text):
 		soup = BeautifulSoup(html_text, "html.parser")
 		if soup("div", test=re.compile("No products found")):
@@ -19,12 +17,13 @@ class WebpageParser:
 			basic_name_element = wine_listing_element.find("h4")
 			bonus_points_element =  wine_listing_element.find("span")
 			price_element = wine_listing_element.find("span", text=lambda value: value and value.startswith("$"))
-
+			if price_element is None:
+				continue
 			wine_deal_list.append(WineDeal(
 				name=name_element.string if name_element else basic_name_element.string,
 				bonus_points=int(bonus_points_element.get_text(strip=True).strip("Earn").strip("Bonus Points").replace(",", "")),
 				price=float(price_element.string.strip("$")),
-				link="https://wine.qantas.com" + wine_listings_element.get("href"),
+				link="https://wine.qantas.com" + wine_listing_element.get("href"),
 			))
 		print(f'Found {len(wine_deal_list)} deals on this page')
 		return wine_deal_list
